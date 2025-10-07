a54e69b722ed3764ba468c4763ecdf0c36d7e9d4
Ellisalean <148759595+Ellisalean@users.noreply.github.com>
2025-10-07 02:07:22 -0400

    Add files via upload

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ export const Header: React.FC = () => {
     return (
         <header className="bg-slate-800 text-white -m-6 sm:-m-8 p-6 sm:p-8 rounded-t-lg flex items-center justify-center relative shadow-lg min-h-[160px]">
             <div className="absolute left-4 sm:left-8 top-1/2 -translate-y-1/2">
-                 <img src={LOGO_URL} alt="Dj Lei Logo" className="h-36 w-36 object-contain" />
+                 <img src={LOGO_URL} alt="Dj Lei Logo" className="h-24 w-24 sm:h-36 sm:w-36 object-contain" />
             </div>
             <div className="text-center">
                 <h1 className="text-3xl font-extrabold text-white tracking-tight">CONTRATO DE SERVICIO</h1>
